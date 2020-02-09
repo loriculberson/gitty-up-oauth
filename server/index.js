3466e094f88632c3091da9fd5ca72152bdb55b21
Lori Culberson <bigskybandit@sbcglobal.net>
2020-02-09 01:05:09 -0500

    omfg configuratio for heroku config again x2

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const passport = require('passport')
 const GitHubStrategy = require('passport-github2').Strategy;
 require('dotenv').config()
